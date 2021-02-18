91260119b1fefa1644b36a8abb29b74fad2ba87d
Jeffrey Regier <regier@umich.edu>
2021-02-18 09:04:46 -0500

    run black

--- spatial/merfish_geometric.py
+++ spatial/merfish_geometric.py
@@ -1,3 +1,4 @@
+import types
 import pandas as pd
 import torch
 import sklearn.neighbors
@@ -6,11 +7,15 @@ import h5py
 import numpy as np
 import requests
 
+
 class Merfish(torch_geometric.data.InMemoryDataset):
-    def __init__(self, root, n_neighbors=3,train=True):
+    def __init__(self, root, n_neighbors=3, train=True):
         super().__init__(root)
 
-        data_list=self.construct_graphs(n_neighbors)
+        data_list = self.construct_graphs(n_neighbors)
+
+        with h5py.File(self.raw_dir + "/merfish.hdf5", "r") as h5f:
+            self.gene_names = h5f["gene_names"][:].astype("U")[~self.bad_genes]
 
         # we use the first 150 slices for training
         if train:
@@ -18,7 +23,7 @@ class Merfish(torch_geometric.data.InMemoryDataset):
         else:
             self.data, self.slices = self.collate(data_list[150:])
 
-    url='https://datadryad.org/stash/downloads/file_stream/68364'
+    url = "https://datadryad.org/stash/downloads/file_stream/68364"
 
     behavior_types = [
         "Naive",
@@ -28,62 +33,86 @@ class Merfish(torch_geometric.data.InMemoryDataset):
         "Aggression to adult",
         "Mating",
     ]
+    behavior_lookup = {x: i for (i, x) in enumerate(behavior_types)}
+
+    bad_genes = np.zeros(161, dtype=np.bool)
+    bad_genes[144] = True
 
     @property
     def raw_file_names(self):
-        return ["merfish.csv","merfish.hdf5"]
-
+        return ["merfish.csv", "merfish.hdf5"]
 
     def download(self):
-        with open(self.raw_dir+'/merfish.csv','wb') as f:
-            f.write(requests.get(self.url).content)
+        with open(self.raw_dir + "/merfish.csv", "wb") as csvf:
+            csvf.write(requests.get(self.url).content)
 
-        df=pd.read_csv(self.raw_dir+"/merfish.csv")
+        dataframe = pd.read_csv(self.raw_dir + "/merfish.csv")
 
-        with h5py.File(self.raw_dir+'/merfish.hdf5','w') as f:
-            for nm,dtype in zip(df.keys()[:9],df.dtypes[:9]):
-                if dtype.kind=='O':
-                    f.create_dataset(nm,data=np.require(df[nm],dtype='S36'))
+        with h5py.File(self.raw_dir + "/merfish.hdf5", "w") as h5f:
+            for colnm, dtype in zip(dataframe.keys()[:9], dataframe.dtypes[:9]):
+                if dtype.kind == "O":
+                    h5f.create_dataset(
+                        colnm, data=np.require(dataframe[colnm], dtype="S36")
+                    )
                 else:
-                    f.create_dataset(nm,data=np.require(df[nm]))
-            f.create_dataset('expression',data=np.array(df[df.keys()[9:]]).astype(np.float16))
-            f.create_dataset('gene_names',data=np.array(df.keys()[9:],dtype='S80'))
+                    h5f.create_dataset(colnm, data=np.require(dataframe[colnm]))
+            h5f.create_dataset(
+                "expression",
+                data=np.array(dataframe[dataframe.keys()[9:]]).astype(np.float16),
+            )
+            h5f.create_dataset(
+                "gene_names", data=np.array(dataframe.keys()[9:], dtype="S80")
+            )
 
-    def construct_graphs(self,n_neighbors):
+    def construct_graph(self, data, anid, breg, n_neighbors):
+        # get subset of cells in this slice
+        good = (data.anids == anid) & (data.bregs == breg)
+
+        # figure out neighborhood structure
+        locations_for_this_slice = data.locations[good]
+        nbrs = sklearn.neighbors.NearestNeighbors(
+            n_neighbors=n_neighbors + 1, algorithm="ball_tree"
+        )
+        nbrs.fit(locations_for_this_slice)
+        _, neighbors = nbrs.kneighbors(locations_for_this_slice)
+        edges = np.concatenate(
+            [np.c_[neighbors[:, 0], neighbors[:, i + 1]] for i in range(n_neighbors)],
+            axis=0,
+        )
+        edges = torch.tensor(edges, dtype=torch.long).T
+
+        # remove gene 144.  which is bad.  for some reason.
+        subexpression = data.expression[good]
+        subexpression = subexpression[:, ~self.bad_genes]
+
+        # get behavior ids
+        behavior_ids = np.array([self.behavior_lookup[x] for x in data.behavior[good]])
+
+        # make it into a torch geometric data object, add it to the list!
+        return torch_geometric.data.Data(
+            x=torch.tensor(subexpression.astype(np.float32)),
+            edge_index=edges,
+            pos=torch.tensor(locations_for_this_slice.astype(np.float32)),
+            y=torch.tensor(behavior_ids),
+        )
+
+    def construct_graphs(self, n_neighbors):
         # load hdf5
-        with h5py.File(self.raw_dir+'/merfish.hdf5','r') as f:
-            anids=f['Animal_ID'][:]
-            bregs=f['Bregma'][:]
-            expression=f['expression'][:]
-            locations=np.c_[f['Centroid_X'][:],f['Centroid_Y'][:]]
-            behavior=f['Behavior'][:].astype("U")
-
-        behavior_lookup={x:i for (i,x) in enumerate(self.behavior_types)}
-        behavior_ids=np.array([behavior_lookup[x] for x in behavior])
+        with h5py.File(self.raw_dir + "/merfish.hdf5", "r") as h5f:
+            data = types.SimpleNamespace(
+                anids=h5f["Animal_ID"][:],
+                bregs=h5f["Bregma"][:],
+                expression=h5f["expression"][:],
+                locations=np.c_[h5f["Centroid_X"][:], h5f["Centroid_Y"][:]],
+                behavior=h5f["Behavior"][:].astype("U"),
+            )
 
         # get the (animal_id,bregma) pairs that define a unique slice
-        unique_slices=np.unique(np.c_[anids,bregs],axis=0)
+        unique_slices = np.unique(np.c_[data.anids, data.bregs], axis=0)
 
         # store all the slices in this list...
-        data_list=[]
-        for anid,breg in unique_slices:
-            # get subset of cells in this slice
-            good=(anids==anid)&(bregs==breg)
-
-            # figure out neighborhood structure
-            locations_for_this_slice=locations[good]
-            nbrs = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors+1, algorithm='ball_tree')
-            nbrs.fit(locations_for_this_slice)
-            distances, neighbors = nbrs.kneighbors(locations_for_this_slice)
-            edges=np.concatenate([np.c_[neighbors[:,0],neighbors[:,i+1]] for i in range(n_neighbors)],axis=0)
-            edges=torch.tensor(edges,dtype=torch.long).T
-
-            # make it into a torch geometric data object, add it to the list!
-            data_list.append(torch_geometric.data.Data(
-                x=expression[good],
-                edge_index=edges,
-                pos=locations_for_this_slice,
-                y=behavior_ids[good]
-            ))
+        data_list = []
+        for anid, breg in unique_slices:
+            data_list.append(self.construct_graph(data, anid, breg, n_neighbors))
 
         return data_list
