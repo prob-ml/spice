6285b9a076c51c41d44edbb5ed6ca045cca9d717
Jeffrey Regier <regier@umich.edu>
2021-02-23 19:43:11 -0500

    now test imports spatial -- which would fail without a GPU

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,7 +1,10 @@
 import pathlib
 
+import spatial
+
 
 def test_merfish_main():
     test_dir = pathlib.Path(__file__).parent.absolute()
     test_data = test_dir.joinpath("data/ten_rows_of_moffitt.csv")
     assert test_data.is_file()
+    assert spatial.__version__ != ""
