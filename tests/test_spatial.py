dae3374574049641efcdb381cb402bf5518e8827
Jackson Loper <jaloper@deeplearning-01.stat.lsa.umich.edu>
2021-02-25 15:38:28 -0500

    Added testfor monet and included "self-edge"-like behavior in gmm base network

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,10 +1,89 @@
 import pathlib
 
+import numpy as np
+import numpy.random as npr
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+import torch_geometric
+
 import spatial
+import spatial.monet_ae
 
 
 def test_merfish_main():
     test_dir = pathlib.Path(__file__).parent.absolute()
     test_data = test_dir.joinpath("data/ten_rows_of_moffitt.csv")
-    assert test_data.is_file()
-    assert spatial.__version__ != ""
+    pd.read_csv(test_data)
+
+
+def test_monetae2d():
+
+    ###################
+    # make simulation
+
+    datalist = []
+
+    # this problem should be pretty easy!
+    data_dimension = 2
+
+    # set random seed
+    npr.seed(0)
+    torch.manual_seed(0)
+
+    # generate random graphs
+    for _ in range(4):
+        # random (but varying!) number of nodes
+        n_nodes = npr.randint(10, 20)
+
+        # fantasize some edges
+        edge_matrix = npr.rand(n_nodes, n_nodes) < 0.2
+        edge_matrix[np.r_[0:n_nodes], np.r_[0:n_nodes]] = False
+        edges = torch.tensor(np.array(np.where(edge_matrix)))
+
+        # 2d positions
+        pos = npr.randn(n_nodes, 2)
+
+        # random data
+        expr = npr.randn(n_nodes, data_dimension)
+
+        datalist.append(
+            torch_geometric.data.Data(
+                x=torch.tensor(expr.astype(np.float32)),
+                edge_index=edges,
+                pos=torch.tensor(pos.astype(np.float32)),
+            )
+        )
+
+    ###################
+    # fitmodel
+
+    latent_dimension = 2
+
+    model = spatial.monet_ae.MonetAutoencoder2D(
+        data_dimension, latent_dimension, loss_type="mse"
+    )
+    trainer = pl.Trainer(gpus=1, max_epochs=5)
+
+    trainer.fit(
+        model,
+        torch_geometric.data.DataLoader(
+            datalist[:2], batch_size=1, num_workers=2, pin_memory=True
+        ),
+        torch_geometric.data.DataLoader(
+            datalist[2:], batch_size=1, shuffle=False, pin_memory=True
+        ),
+    )
+
+    ###################
+    # check our performance on a training example
+
+    # get loss if our network outputs all zeros
+    loss_if_we_are_dumb = np.sum(datalist[0].x.detach().cpu().numpy() ** 2)
+
+    # get loss from trained network
+    _, recon = model(datalist[0])
+    loss = model.calc_loss(recon, datalist[0].x)
+
+    # make sure we are doing better than the trivial answer
+    assert loss.detach().cpu() < 0.3 * loss_if_we_are_dumb
