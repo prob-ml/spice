5f0441660d001fc5a396dc13cf0352d43223fa6d
roromaniac <58533981+roromaniac@users.noreply.github.com>
2021-06-01 15:04:13 -0500

    spatial AE accuracy check added (#45)
    * spatial AE accuracy check added
    
    * increased code cov and better tests
    
    * environment variable test in yml
    
    * fix utilities issue when running pytests on Github
    
    * Roman doesn't know how to spell
    
    Co-authored-by: Roman Daniel Kouznetsov <roko@deeplearning-01.stat.lsa.umich.edu>

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,9 +1,9 @@
 import pathlib
 
 import numpy as np
-import pytorch_lightning as pl
 import torch
 import torch_geometric
+from hydra.experimental import compose, initialize
 from numpy import random as npr
 
 
@@ -16,12 +16,7 @@ def test_merfish_dataset():
     mfd.get(0)
 
 
-def test_monetae2d():
-    from spatial.models import monet_ae
-
-    ###################
-    # make simulation
-
+def simulate_data():
     datalist = []
 
     # this problem should be pretty easy!
@@ -32,7 +27,7 @@ def test_monetae2d():
     torch.manual_seed(0)
 
     # generate random graphs
-    for _ in range(4):
+    for _ in range(12):
         # random (but varying!) number of nodes
         n_nodes = npr.randint(10, 20)
 
@@ -55,41 +50,88 @@ def test_monetae2d():
             )
         )
 
+    return datalist, data_dimension
+
+
+def test_monetae2d():
+    from spatial import train
+
     ###################
-    # fitmodel
+    # make simulation
+    simulated_data, data_dimension = simulate_data()
 
-    hidden_dimensions = [100, 50, 25, 10]
-    latent_dimension = 2
+    ###################
+    # fitmodel with updated hydra config
 
-    model = monet_ae.MonetAutoencoder2D(
-        data_dimension,
-        hidden_dimensions,
-        latent_dimension,
-        dim=2,
-        kernel_size=25,
-        loss_type="mse",
-    )
-    trainer = pl.Trainer(gpus=1, max_epochs=5)
+    overrides = {
+        "gpus": 0,
+        "model": "MonetAutoencoder2D",
+        "model.kwargs.observables_dimension": data_dimension,
+        "model.kwargs.hidden_dimensions": [100, 50, 25, 10],
+        "model.kwargs.latent_dimension": 2,
+        "training.n_epochs": 10,
+    }
+    overrides_list = [f"{k}={v}" for k, v in overrides.items()]
 
-    trainer.fit(
-        model,
-        torch_geometric.data.DataLoader(
-            datalist[:2], batch_size=1, num_workers=2, pin_memory=True
-        ),
-        torch_geometric.data.DataLoader(
-            datalist[2:], batch_size=1, shuffle=False, pin_memory=True
-        ),
-    )
+    with initialize(config_path="../config"):
+        cfg = compose(config_name="config", overrides=overrides_list)
+        trained_model = train.train(cfg, data=simulated_data)
 
     ###################
     # check our performance on a training example
 
     # get loss if our network outputs all zeros
-    loss_if_we_are_dumb = np.sum(datalist[0].x.detach().cpu().numpy() ** 2)
+    loss_if_we_are_dumb = np.sum(simulated_data[0].x.detach().cpu().numpy() ** 2)
 
     # get loss from trained network
-    _, recon = model(datalist[0])
-    loss = model.calc_loss(recon, datalist[0].x)
+    _, recon = trained_model(simulated_data[0])
+    loss = trained_model.calc_loss(recon, simulated_data[0].x)
 
     # make sure we are doing better than the trivial answer
-    assert loss.detach().cpu() < 0.3 * loss_if_we_are_dumb
+    assert loss.detach().cpu() < 0.4 * loss_if_we_are_dumb
+
+    return loss
+
+
+def test_trivial():
+    from spatial import train
+
+    ###################
+    # make simulation
+    simulated_data, data_dimension = simulate_data()
+
+    ###################
+    # fitmodel with updated hydra config
+
+    overrides = {
+        "gpus": 0,
+        "model": "TrivialAutoencoder",
+        "model.kwargs.observables_dimension": data_dimension,
+        "model.kwargs.hidden_dimensions": [100, 50, 25, 10],
+        "model.kwargs.latent_dimension": 2,
+        "training.n_epochs": 10,
+    }
+    overrides_list = [f"{k}={v}" for k, v in overrides.items()]
+
+    with initialize(config_path="../config"):
+        cfg = compose(config_name="config", overrides=overrides_list)
+        trained_model = train.train(cfg, data=simulated_data)
+
+    ###################
+    # check our performance on a training example
+
+    # get loss if our network outputs all zeros
+    loss_if_we_are_dumb = np.sum(simulated_data[0].x.detach().cpu().numpy() ** 2)
+
+    # get loss from trained network
+    _, recon = trained_model(simulated_data[0])
+    loss = trained_model.calc_loss(recon, simulated_data[0].x)
+
+    # make sure we are doing better than the trivial answer
+    assert loss.detach().cpu() < 0.4 * loss_if_we_are_dumb
+
+    return loss
+
+
+def test_accuracy():
+    assert test_monetae2d() < 0.85 * test_trivial()
