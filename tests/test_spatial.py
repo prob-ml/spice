d045927e4b8b60d0ba8695f2fc9e869660386b44
Roman Daniel Kouznetsov <roko@deeplearning-01.stat.lsa.umich.edu>
2021-06-01 11:18:11 -0400

    spatial AE accuracy check added

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -16,12 +16,7 @@ def test_merfish_dataset():
     mfd.get(0)
 
 
-def test_monetae2d():
-    from spatial.models import monet_ae
-
-    ###################
-    # make simulation
-
+def simulate_data():
     datalist = []
 
     # this problem should be pretty easy!
@@ -55,6 +50,16 @@ def test_monetae2d():
             )
         )
 
+    return datalist, data_dimension
+
+
+def test_monetae2d():
+    from spatial.models import monet_ae
+
+    ###################
+    # make simulation
+    simulated_data, data_dimension = simulate_data()
+
     ###################
     # fitmodel
 
@@ -74,10 +79,10 @@ def test_monetae2d():
     trainer.fit(
         model,
         torch_geometric.data.DataLoader(
-            datalist[:2], batch_size=1, num_workers=2, pin_memory=True
+            simulated_data[:2], batch_size=1, num_workers=2, pin_memory=True
         ),
         torch_geometric.data.DataLoader(
-            datalist[2:], batch_size=1, shuffle=False, pin_memory=True
+            simulated_data[2:], batch_size=1, shuffle=False, pin_memory=True
         ),
     )
 
@@ -85,11 +90,64 @@ def test_monetae2d():
     # check our performance on a training example
 
     # get loss if our network outputs all zeros
-    loss_if_we_are_dumb = np.sum(datalist[0].x.detach().cpu().numpy() ** 2)
+    loss_if_we_are_dumb = np.sum(simulated_data[0].x.detach().cpu().numpy() ** 2)
 
     # get loss from trained network
-    _, recon = model(datalist[0])
-    loss = model.calc_loss(recon, datalist[0].x)
+    _, recon = model(simulated_data[0])
+    loss = model.calc_loss(recon, simulated_data[0].x)
 
     # make sure we are doing better than the trivial answer
     assert loss.detach().cpu() < 0.3 * loss_if_we_are_dumb
+
+    return loss
+
+
+def test_trivial():
+    from spatial.models import monet_ae
+
+    ###################
+    # make simulation
+    simulated_data, data_dimension = simulate_data()
+
+    ###################
+    # fitmodel
+
+    hidden_dimensions = [100, 50, 25, 10]
+    latent_dimension = 2
+
+    model = monet_ae.TrivialAutoencoder(
+        data_dimension,
+        hidden_dimensions,
+        latent_dimension,
+        loss_type="mse",
+    )
+    trainer = pl.Trainer(gpus=1, max_epochs=5)
+
+    trainer.fit(
+        model,
+        torch_geometric.data.DataLoader(
+            simulated_data[:2], batch_size=1, num_workers=2, pin_memory=True
+        ),
+        torch_geometric.data.DataLoader(
+            simulated_data[2:], batch_size=1, shuffle=False, pin_memory=True
+        ),
+    )
+
+    ###################
+    # check our performance on a training example
+
+    # get loss if our network outputs all zeros
+    loss_if_we_are_dumb = np.sum(simulated_data[0].x.detach().cpu().numpy() ** 2)
+
+    # get loss from trained network
+    _, recon = model(simulated_data[0])
+    loss = model.calc_loss(recon, simulated_data[0].x)
+
+    # make sure we are doing better than the trivial answer
+    assert loss.detach().cpu() < 0.4 * loss_if_we_are_dumb
+
+    return loss
+
+
+def test_accuracy():
+    assert test_monetae2d() < test_trivial()
