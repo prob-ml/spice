8286236002d118b7c33b2bcb497649e4d17bd75a
roromaniac <58533981+roromaniac@users.noreply.github.com>
2021-07-27 14:49:41 -0500

    Update train.py

--- spatial/train.py
+++ spatial/train.py
@@ -62,8 +62,8 @@ def train(cfg: DictConfig, data=None):
     train_n = round(n_data * 11 / 12)
     train_data, val_data = random_split(data, [train_n, n_data - train_n])
 
-    train_loader = DataLoader(train_data, batch_size=1, num_workers=2)
-    val_loader = DataLoader(val_data, batch_size=1, num_workers=2)
+    train_loader = DataLoader(train_data, batch_size=4, num_workers=2)
+    val_loader = DataLoader(val_data, batch_size=4, num_workers=2)
 
     # setup trainer
     trainer_dict = OmegaConf.to_container(cfg.training.trainer, resolve=True)
