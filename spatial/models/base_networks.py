69a38995b4764f01c7e4ebcc3f036138892f0da0
Roman Daniel Kouznetsov <roko@gl-login1.arc-ts.umich.edu>
2023-11-15 20:30:40 -0500

    great lakes code

--- spatial/models/base_networks.py
+++ spatial/models/base_networks.py
@@ -64,39 +64,64 @@ class DenseReluGMMConvNetwork(torch.nn.Module):
     def forward(self, vals, edges, pseudo, num_gpus=4):
         orig_vals = []
         num_layers = len(self.gmms)
+
+        ## MANUAL SHARDING
+        # for i, (dense, gmmlayer) in enumerate(zip(self.linears, self.gmms)):
+        #     # move all the layers to the appropriate device
+        #     gpu_to_use = (
+        #         f"cuda:{int(i // (num_layers / num_gpus))}"
+        #         if num_gpus > 1
+        #         else "cuda:0"
+        #     )
+        #     # create an identity copy if we need a residual block
+        #     vals = vals.to(gpu_to_use)
+        #     if self.include_skip_connections:
+        #         orig_vals.append(vals.clone())
+        #     edges = edges.to(gpu_to_use)
+        #     pseudo = pseudo.to(gpu_to_use)
+        #     gmmlayer = gmmlayer.to(gpu_to_use)
+        #     dense = dense.to(gpu_to_use)
+        #     self.batchnorms[i] = self.batchnorms[i].to(gpu_to_use)
+
+        #     if self.dropout:
+        #         vals = self.dropouts(vals).to(gpu_to_use)
+        #     # adj = SparseTensor(row=edges[0], col=edges[1], value=pseudo)
+        #     # vals = gmmlayer(vals, adj.t()) + dense(vals)
+        #     vals = gmmlayer(vals, edges, pseudo) + dense(vals)
+        #     if i >= 2 and self.include_skip_connections:
+        #         for j in range(i + 1):
+        #             if self.sizes[j] == self.sizes[i + 1]:
+        #                 vals += orig_vals[j].to(gpu_to_use)
+
+        #     # do batchnorm
+        #     if self.use_batchnorm:
+        #         vals = self.batchnorms[i](vals.float().to(gpu_to_use))
+
+        #     # do relu (or not, if final_relu=False and we're on the last layer)
+        #     if self.final_relu or (i != len(self.gmms) - 1):
+        #         vals = fcl.relu(vals.float()).to(gpu_to_use)
+
+        ## NO MANUAL SHARDING, SHOULD BE TAKEN CARE OF BY STRATEGY
         for i, (dense, gmmlayer) in enumerate(zip(self.linears, self.gmms)):
-            # move all the layers to the appropriate device
-            gpu_to_use = (
-                f"cuda:{int(i // (num_layers / num_gpus))}"
-                if num_gpus > 1
-                else "cuda:0"
-            )
-            # create an identity copy if we need a residual block
-            vals = vals.to(gpu_to_use)
             if self.include_skip_connections:
                 orig_vals.append(vals.clone())
-            edges = edges.to(gpu_to_use)
-            pseudo = pseudo.to(gpu_to_use)
-            gmmlayer = gmmlayer.to(gpu_to_use)
-            dense = dense.to(gpu_to_use)
-            self.batchnorms[i] = self.batchnorms[i].to(gpu_to_use)
 
             if self.dropout:
-                vals = self.dropouts(vals).to(gpu_to_use)
+                vals = self.dropouts(vals)
             # adj = SparseTensor(row=edges[0], col=edges[1], value=pseudo)
             # vals = gmmlayer(vals, adj.t()) + dense(vals)
             vals = gmmlayer(vals, edges, pseudo) + dense(vals)
             if i >= 2 and self.include_skip_connections:
                 for j in range(i + 1):
                     if self.sizes[j] == self.sizes[i + 1]:
-                        vals += orig_vals[j].to(gpu_to_use)
+                        vals += orig_vals[j]
 
             # do batchnorm
             if self.use_batchnorm:
-                vals = self.batchnorms[i](vals.float().to(gpu_to_use))
+                vals = self.batchnorms[i](vals.float())
 
             # do relu (or not, if final_relu=False and we're on the last layer)
             if self.final_relu or (i != len(self.gmms) - 1):
-                vals = fcl.relu(vals.float()).to(gpu_to_use)
+                vals = fcl.relu(vals.float())
 
         return vals
